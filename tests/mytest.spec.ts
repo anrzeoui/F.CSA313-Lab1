7ad694eb6970312196c7797a2d8fa2480ca12c60
anrzeoui <anar.urnukhbayar@gmail.com>
2026-09-08 14:46:31 +0800

    Saucedemo test

--- tests/mytest.spec.ts
+++ tests/mytest.spec.ts
@@ -3,7 +3,6 @@ import { test, expect} from '@playwright/test';
 test('Амжилттай нэвтрэх', async({page}) => {
     // saucedemo site g neeh
     await page.goto('https://www.saucedemo.com');
-
     //username password oruulah
     await page.getByPlaceholder('Username').fill('standard_user');
     await page.getByPlaceholder('Password').fill('secret_sauce');
@@ -20,7 +19,6 @@ test('Амжилттай нэвтрэх', async({page}) => {
 test('Амжилтгүй нэвтрэх', async({page}) => {
     // saucedemo site g neeh
     await page.goto('https://www.saucedemo.com');
-
     //username password oruulah
     await page.getByPlaceholder('Username').fill('standard_user');
     await page.getByPlaceholder('Password').fill('wrong_pass');
@@ -33,7 +31,6 @@ test('Амжилтгүй нэвтрэх', async({page}) => {
 test('Нэвтэрсний дараа бараа сагслах', async({page}) => {
     // saucedemo site g neeh
     await page.goto('https://www.saucedemo.com');
-
     //username password oruulah
     await page.getByPlaceholder('Username').fill('standard_user');
     await page.getByPlaceholder('Password').fill('secret_sauce');
@@ -47,6 +44,4 @@ test('Нэвтэрсний дараа бараа сагслах', async({page}) 
     await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
     await page.getByRole('button', {name: 'Open Menu'}).click();
     await page.getByText('Logout').click();
-
-
 });
